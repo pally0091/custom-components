ca62cf8dc5164c1243baf6d6570818413b1175fe
Debashis Roy <goodquote39@hotmail.com>
2023-11-07 16:55:57 +0600

    custom button add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
 import Image from "next/image";
+import Button from "@/components/Button";
 
 export default function Home() {
   return (
     <main className="text-center">
       <h1 className="text-2xl font-bold">Components</h1>
+      <Button
+        secondary
+        label="secondary"
+      ></Button>
+      <Button
+        primary
+        label="primary"
+      ></Button>
+      <br />
+      <div className="w-56 mx-auto">
+        <Button
+          label="Button"
+          full
+        ></Button>
+      </div>
     </main>
   );
 }
---
+++ src/components/Button.tsx
@@ -0,0 +1,68 @@
+"use client";
+import React, { useState, CSSProperties, ReactNode, MouseEvent } from "react";
+
+interface ButtonProps {
+  children?: ReactNode;
+  label?: string;
+  primary?: boolean;
+  secondary?: boolean;
+  full?: boolean;
+  className?: string;
+  onClick?: (event: MouseEvent<HTMLButtonElement>) => void;
+}
+
+const Button: React.FC<ButtonProps> = ({
+  children,
+  label,
+  primary,
+  secondary,
+  full,
+  onClick,
+  className,
+}: ButtonProps) => {
+  const [backgroundColor, setBackgroundColor] = useState<string>(
+    primary ? "#FF6F61" : secondary ? "white" : "white"
+  );
+  const [color, setColor] = useState<string>(
+    primary ? "white" : secondary ? "black" : "#ff6f61"
+  );
+
+  const buttonStyle: CSSProperties = {
+    padding: "8px 15px",
+    borderRadius: "8px",
+    width: full ? "100%" : "auto",
+    cursor: "pointer",
+    fontSize: "16px",
+    fontWeight: "bold",
+    margin: "5px",
+    textAlign: "center",
+    color,
+    backgroundColor,
+    border: "1px solid #D1D5DB",
+    transition: "background-color 0.5s",
+  };
+
+  const handleHover = () => {
+    setColor(primary ? "black" : secondary ? "white" : "black");
+    setBackgroundColor(primary ? "white" : secondary ? "#FF6F61" : "lightgray");
+  };
+
+  const handleLeave = () => {
+    setColor(primary ? "white" : secondary ? "black" : "#ff6f61");
+    setBackgroundColor(primary ? "#FF6F61" : secondary ? "white" : "white");
+  };
+
+  return (
+    <button
+      className={className}
+      style={buttonStyle}
+      onClick={onClick}
+      onMouseEnter={handleHover}
+      onMouseLeave={handleLeave}
+    >
+      {label} {children}
+    </button>
+  );
+};
+
+export default Button;
